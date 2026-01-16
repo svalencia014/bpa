48f78d5b2f4c71294ca8bae8487d09c6e0f1389a
hucu314 <hcumpean@ftstudent.org>
2026-01-16 08:52:48 -0500

    login and home

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -5,18 +5,58 @@
 <!-- styles -->
 <style>
     .nav {
+        display:flex;
+        justify-content:space-between;
+        align-content:center;
         width: 100%;
         background-color:gray;
         height:107px;
+        background-color:#EEEEEE;
     }
     body{
         margin:0px;
         padding:0px;
         border:0px;
     }
+
+    .logo {
+        background-image:url('https://codehs.com/uploads/299008d9b4f2bcc709c4fba112cec76e');
+        background-size:contain;
+        width:239px;
+        height:99px;
+        margin-left:7px;
+        margin-top:4px;
+        margin-bottom:4px;
+    }
+
+    .rnav {
+        display:flex;
+        align-content:center;
+        border-radius:19px;
+        background-color:white;
+        width:144px;
+        height:72px;
+        margin-top:18px;
+        margin-bottom:17px;
+        margin-right:20px;
+    }
+
+    .login-btn {
+        width:144px;
+        height:72px;
+        background-color:#312C85;
+        color:#EEEEEE;
+        font-family:Arial;
+        font-size:32px;
+        border-radius:19px;
+    }
 </style>
 
 <!-- html -->
- <body>
-    <div class='nav'></div>
- </body>
+<div class='nav'>
+    <div class='logo'>
+    </div>
+    <div class='rnav'>
+        <button class='login-btn'>Login</button>
+    </div>
+</div>
